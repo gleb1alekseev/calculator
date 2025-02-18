cc07e0b0981187b5aacfdeb9e17f95fb7707a6f5
g.alekseev <gleb1alekseev@gmail.com>
2025-02-18 19:04:45 +0300

    lesson21 update

--- src/main/java/calcPackage/Calculator.java
+++ src/main/java/calcPackage/Calculator.java
@@ -2,22 +2,22 @@ package calcPackage;
 
 public class Calculator {
     public double sum(double a, double b){
-        System.out.println("sum" + a + "and" + b + "equals" + (a + b));
+        System.out.println("sum" + a + "and" + b + "=" + (a + b));
         return a + b;
     }
 
     public double subtract(double a, double b){
-        System.out.println("subtraction" + a + "and" + b + "equals" + (a - b));
+        System.out.println("subtraction" + a + "and" + b + "=" + (a - b));
         return a - b;
     }
 
     public double multiply(double a, double b){
-        System.out.println("multiplication" + a + "and" + b + "equals" + (a * b));
+        System.out.println("multiplication" + a + "and" + b + "=" + (a * b));
         return a * b;
     }
 
     public double divide(double a, double b){
-        System.out.println("division" + a + "and" + b + "equals" + (a / b));
+        System.out.println("division" + a + "and" + b + "=" + (a / b));
         if (b == 0){
             throw new ArithmeticException("division by zero");
         }
